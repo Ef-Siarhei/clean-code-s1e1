cb2ae6f1047465057b21468e6c941ec2c14e0b81
Siarhei Yafimenka <h1_n1@mail.ru>
2023-04-19 23:01:44 +0300

    refactor(basic-3.4): change tag selectors

--- app.js
+++ app.js
@@ -32,18 +32,20 @@ var createNewTaskElement = function (taskString) {
   var deleteButton = document.createElement("button");//delete button
   var deleteButtonImg = document.createElement("img");//delete button image
 
+  listItem.className = "todo__list";
   label.innerText = taskString;
-  label.className = "task";
+  label.className = "task task__label";
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
+  checkBox.className = "checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  editInput.className = "task task__input";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "edit button";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "delete button";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
